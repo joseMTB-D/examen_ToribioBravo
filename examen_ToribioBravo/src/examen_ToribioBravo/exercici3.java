db2b926ab2e18ebaa299dd98c819ff5ff940547f
joseMTB-D <59867779+joseMTB-D@users.noreply.github.com>
2020-02-28 09:32:53 +0100

    Add files via upload

---
+++ examen_ToribioBravo/src/examen_ToribioBravo/exercici3.java
@@ -0,0 +1,16 @@
+package examen_ToribioBravo;
+
+public class exercici3 {
+	public static void main (String[] args) {
+		printOne();
+		printOne();
+		printTwo();
+	}
+public static void printOne() {
+	System.out.println("Hello World");
+}
+public static void printTwo() {
+	printOne();
+	printOne();
+}
+}
---
+++ examen_ToribioBravo/src/module-info.java
@@ -0,0 +1,2 @@
+module examen_ToribioBravo {
+}
